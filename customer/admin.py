e35e0d5121af1ce9e565330590079ca004eb2c25
saicharandeepbandu <74974959+saicharandeepbandu@users.noreply.github.com>
2024-02-23 11:14:41 +0530

    data being stored into data

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import CustomerInfo
 # Register your models here.
+admin.site.register(CustomerInfo)
--- plan/views.py
+++ plan/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
 from django import forms
-from  plan import forms
-
+from  plan import forms as planForms
+from plan import models as planModels
 # Create your views here.
 
 def plan_info_view(request):
-    form=forms.pl_info()
+    form=planForms.pl_info()
     if request.method == "POST":
-        form=Customer_info(request.POST)
+        form=planForms.pl_info(request.POST)
         if form.is_valid():
-            form.save(commit=True)
+            pn=form.cleaned_data['plan_name']
+            no=form.cleaned_data['no_of_members']
+            dur=form.cleaned_data['duration']
+            amt=form.cleaned_data['amount']
+            data=planModels.plan_info(plan_name=pn,no_of_members=no,duration=dur,amount=amt)
+            data.save()
             
 
         else:
-             form = pl_info()
+             form = planForms.pl_info()
     return render(request, 'customer/form_plan.html', {'form': form})
 # Create your views here.
