eaa3b25f8b76948730bd1dee9abf970a6d630595
vsuohu <vvsuohu@gmail.com>
2024-03-25 14:32:56 +0530

    init delete contact

--- contact/urls.py
+++ contact/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from contact import views
+from contact import views, actions
 
 app_name = "contact"
 
 urlpatterns = [
     path("list/", views.ContactListView.as_view(), name="list"),
     path("create/", views.ContactCreateView.as_view(), name="create"),
-    path("edit/<uuid:contact_id>/", views.ContactEditView.as_view(), name="edit")
+    path("edit/<uuid:contact_id>/", views.ContactEditView.as_view(), name="edit"),
+
+    path("delete/<uuid:contact_id>/", actions.delete_contact.as_view(), name="delete"),
 ]
