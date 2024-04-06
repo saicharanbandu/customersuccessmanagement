6356866df55fbbe1466be1dee8f95efc97b4d886
vsuohu <vvsuohu@gmail.com>
2024-04-06 18:43:13 +0530

    view customer info

--- customer/actions.py
+++ customer/actions.py
@@ -7,9 +7,9 @@ from . import models as customerModels
 
 @login_required
 def get_customer_info(request, customer_id):
-    template = 'customer/customer_info.html'
+    template = 'customer/_partials/_info_view.html'
     customer_profile = customerModels.Profile.objects.get(uuid=customer_id)
     context = {
-        'customer_profile': customer_profile,
+        'customer': customer_profile,
     }
     return render(request, template, context)
